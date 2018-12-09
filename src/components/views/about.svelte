dbb3a8ff45b4d8290cd0babf41879abc9fb6464c
Simon Layfield <simonlayfield@simons-mbp.home>
2018-12-09 22:33:48 +0000

    Formal release

--- src/components/views/about.svelte
+++ src/components/views/about.svelte
@@ -14,6 +14,7 @@
     <h1>What is The Void?</h1>
     <p>Abstract images, by nature, are open to interpretation. Two different people may look at the same composition and see and connect with totally different aspects.</p>
     <p><strong>The Void</strong> is an experiment that encourages public contributions to short stories, fueled by the mystery and suspense of purposefully composed abstract ink compositions.</p>
+    <!-- <p>By challenging and exploring our imaginations we </p> -->
     <p><a href="/dashboard">&lsaquo; Head back</a> to the void</p>
   </div>
 
